f3726fda0f571375cce82f3c6c995dc8cfaf1b28
Kent <kentwalden1@gmail.com>
2025-07-12 10:36:12 -0400

    finished exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = (word, num) => {
+    if (num < 0) return "ERROR";
+    
+    let repeat = "";
+    for (let i = 0; i < num; i++){
+        repeat += word;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
