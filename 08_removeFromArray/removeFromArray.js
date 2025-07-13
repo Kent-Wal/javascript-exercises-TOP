1b3c288616ffdfb73330d7a3245682d842bbc04f
Kent <kentwalden1@gmail.com>
2025-07-13 09:02:04 -0400

    finished exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, a, b = '', c = '', d = '') => {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === a || arr[i] === b || arr[i] === c || arr[i] === d) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
