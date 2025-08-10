b19fdf5b629131adf5094c4978e4d9c4479a9626
Kent <kentwalden1@gmail.com>
2025-08-10 18:54:42 -0400

    finished exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (seriesIndex) {
+    seriesIndex = parseInt(seriesIndex);
 
+    let previous = 1;
+    let current = 1;
+    let next;
+
+    if (seriesIndex === 0) return 0;
+    if (seriesIndex < 0) return 'OOPS';
+
+    for (let i = 0; i < seriesIndex - 2; i++){
+        next = previous + current;
+        previous = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
