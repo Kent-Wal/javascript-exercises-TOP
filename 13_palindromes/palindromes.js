5db7a31fbec85606da736c7e5821bc1be5a8cc79
Kent <kentwalden1@gmail.com>
2025-08-10 18:44:32 -0400

    finished exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const wordCopy = Array.from(word);
+    let wordReverse = "";
 
+    //reverse wordCopy
+    for (let i = 0; i < word.length; i++){
+        wordReverse += wordCopy[word.length - 1 - i];
+    }
+
+    return wordReverse === word
 };
 
 // Do not edit below this line
