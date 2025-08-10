052fc66703e2d30296c2ba4b23cfb6cb81645ed8
Kent <kentwalden1@gmail.com>
2025-08-10 19:30:43 -0400

    finished exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = 0;
+    let oldestIndex;
 
-};
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath == undefined) {
+            const date = new Date();
+            people[i].yearOfDeath = date.getFullYear();
+        }
 
+        let age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+        if (oldest < age) {
+            oldest = age;
+            oldestIndex = i;
+        } 
+    }
+
+    return people[oldestIndex];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
